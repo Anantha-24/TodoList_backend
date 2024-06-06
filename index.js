9b5bbb5caac12f0da1d42225d62092cd4f1845ce
Ananthakrishnan <126595058+Anantha-24@users.noreply.github.com>
2024-06-07 00:29:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/todolist', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  
 })
 .then(() => {
   console.log('Connected to MongoDB');
